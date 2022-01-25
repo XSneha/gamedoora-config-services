ba5b134e773dc0ca2a0a2c7a8469ebee18490caf
Sneha <35762143+XSneha@users.noreply.github.com>
2022-01-25 20:28:02 +0530

    changes for hosting application properties

---
+++ src/main/java/com/gamedoora/gamedoora/config/services/GamedooraConfigServer.java
@@ -0,0 +1,15 @@
+package com.gamedoora.gamedoora.config.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@SpringBootApplication
+@EnableConfigServer
+public class GamedooraConfigServer {
+  public static void main(String[] args) {
+    SpringApplication.run(GamedooraConfigServer.class, args);
+  }
+}
+
